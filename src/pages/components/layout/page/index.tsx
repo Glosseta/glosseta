892726c0a5718399b9148acfe848d70ab0348d99
Narbeh Shahnazarian <narbwork@gmail.com>
2021-11-09 13:14:11 -0800

    minor styling tweak v6

--- src/pages/components/layout/page/index.tsx
+++ src/pages/components/layout/page/index.tsx
@@ -5,8 +5,16 @@ import Footer from "../../footer/footer";
 const PageLayout = ({ children }: { children?: object }): JSX.Element => {
   return (
     <Stack
+      spacing={10}
       background="#7a08fc"
-      minHeight="100vh" 
+      padding={1}
+      display="flex"
+      justifyContent="center"
+      flexDirection="column"
+      alignItems="center"
+      minHeight="100vh"
+      minWidth="100vw"
+      flex="1"
     >
       {children}
       <Footer />
