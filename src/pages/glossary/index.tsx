8d5644436f44960af6a771b6920d4ee1d26143a1
Narbeh Shahnazarian <narbwork@gmail.com>
2021-11-28 11:44:45 -0800

    Fixes styling issues with ResultBoxes being to wide on desktop

--- src/pages/glossary/index.tsx
+++ src/pages/glossary/index.tsx
@@ -1,12 +1,21 @@
 import PageLayout from "../components/layout/page";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import getTermList from "../../utils/termListUtil";
-import { SimpleGrid, chakra } from "@chakra-ui/react";
+import {
+  SimpleGrid,
+  chakra,
+  Heading,
+  VStack,
+  Container,
+} from "@chakra-ui/react";
 import { ResultBox } from "../search/result-box";
 import { NewTermRequest } from "./new-term-request";
 import { ScrollToTopButton } from "./scroll-to-top-button";
+import { useTranslation } from "react-i18next";
 
 const AllTerms = ({ terms }: any): JSX.Element => {
+  const { t } = useTranslation();
+
   /**
    * TODO:
    * 1. Add a return for if the terms map is empty
@@ -16,6 +25,15 @@ const AllTerms = ({ terms }: any): JSX.Element => {
     <>
       <PageLayout>
         <chakra.main>
+          <Heading
+            as="h1"
+            padding={5}
+            color="white"
+            fontSize={{ base: "xl", sm: "3xl" }}
+            textAlign="center"
+          >
+            {t("glossaryPageHeading")}
+          </Heading>
           <SimpleGrid
             columns={1}
             spacing={5}
@@ -24,19 +42,23 @@ const AllTerms = ({ terms }: any): JSX.Element => {
             flexDirection="column"
             display="flex"
             alignItems="center"
-          >     
-            <NewTermRequest/>
-            {terms.map((termItem: any) => {
-              return (
-                <ResultBox
-                  key={termItem.term}
-                  definition={termItem.definition}
-                  category={termItem.category.toUpperCase()}
-                  term={termItem.term.toUpperCase()}
-                />
-              );
-            })}
-            <ScrollToTopButton />
+          >
+            <Container maxW={{ base: "sm", sm: "xl" }}>
+              <VStack spacing={5}>
+                {terms.map((termItem: any) => {
+                  return (
+                    <ResultBox
+                      key={termItem.term}
+                      definition={termItem.definition}
+                      category={termItem.category.toUpperCase()}
+                      term={termItem.term.toUpperCase()}
+                    />
+                  );
+                })}
+                <ScrollToTopButton />
+                <NewTermRequest />
+              </VStack>
+            </Container>
           </SimpleGrid>
         </chakra.main>
       </PageLayout>
