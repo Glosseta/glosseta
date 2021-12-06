a61c85892c8cb1a4304384844d5d147341208387
Narbeh Shahnazarian <narbwork@gmail.com>
2021-12-05 16:55:03 -0800

    Adds main nav test suite

---
+++ _tests_/components/nav/index.test.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import Nav from "../../../src/pages/components/nav/index";
+
+jest.mock("react-i18next", () => ({
+  useTranslation: () => ({ t: (key) => key }),
+}));
+
+jest.spyOn(require("next/router"), "useRouter").mockImplementation(() => ({
+  pathname: jest.fn(),
+}));
+
+describe("Nav", () => {
+  it("It renders the Navigation bar", () => {
+    render(<Nav />);
+
+    const nav = screen.getByTitle("glosseta-nav");
+    const glossetaIcon = screen.getByTitle("nav-glosseta-icon");
+    const homeButton = screen.queryByTitle("nav-glosseta-home-button");
+
+    expect(nav).toBeInTheDocument();
+    expect(homeButton).toBeInTheDocument();
+    expect(homeButton).toHaveTextContent("glossetaTitle");
+    expect(glossetaIcon).toBeInTheDocument();
+  });
+});
