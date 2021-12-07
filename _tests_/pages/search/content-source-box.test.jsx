42be3671121ff825e29b48f5f53f72b0387315e7
Narbeh Shahnazarian <narbwork@gmail.com>
2021-12-06 22:16:38 -0800

    Adds test for content-source-box

---
+++ _tests_/pages/search/content-source-box.test.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import ContentSourceBox from "../../../src/pages/search/content-source-box";
+
+jest.mock("react-i18next", () => ({
+  useTranslation: () => ({ t: (key) => key }),
+}));
+
+describe("Content source box", () => {
+  it("It renders the content box with expected content", () => {
+    const transactionId = "12345";
+
+    render(<ContentSourceBox transactionId={transactionId} />);
+
+    const contentSourceHeading = screen.getByText(
+      "searchResultContentSourceHeading"
+    );
+    const contentSourceDescription = screen.getByText(
+      "searchResultContentSourceDescription"
+    );
+    const contentSourceViewBlockUrl = screen.getByText(
+      "searchResultContentSourceTransactionLinkText"
+    );
+    const contentSourceA11yText = screen.getByText("opensInANewWindow");
+
+    // No need to test the text content as it's implicitly tested while trying to find the element above
+    expect(contentSourceHeading).toBeInTheDocument();
+    expect(contentSourceDescription).toBeInTheDocument();
+    expect(contentSourceViewBlockUrl).toBeInTheDocument();
+    expect(contentSourceA11yText).toBeInTheDocument();
+  });
+});
