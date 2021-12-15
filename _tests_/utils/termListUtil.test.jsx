07ccfce1feeb7ccffb32721bb015b07e1342f4d5
Narbeh Shahnazarian <narbwork@gmail.com>
2021-12-14 18:53:36 -0800

    Adds test for termListUtils

---
+++ _tests_/utils/termListUtil.test.jsx
@@ -0,0 +1,12 @@
+import setimmediate from "setimmediate";
+import getTermList from "../../src/utils/termListUtil";
+
+// TODO: See if there's a better way to test this util, mocking fast-csv seems to be non-trivial and will require a bit more investigation
+describe("Tests building glossary terms", () => {
+  it("Reads and returns data from CSV", async () => {
+    const resp = await getTermList("en");
+
+    expect(resp).not.toBeNull();
+    expect(resp.length > 0).toBeTruthy();
+  });
+});
