ffcaf3e77e74c22acc272e2e17f820224bbd4f10
Narbeh Shahnazarian <narbwork@gmail.com>
2022-01-01 13:23:31 -0800

    Adds/modifies unit tests for search-bar

--- _tests_/pages/search/search-bar.test.jsx
+++ _tests_/pages/search/search-bar.test.jsx
@@ -1,10 +1,16 @@
 import React from "react";
-import { render, screen, fireEvent } from "@testing-library/react";
+import { render, screen, fireEvent, createEvent } from "@testing-library/react";
 import SearchBar from "../../../src/pages/search/search-bar";
 
 jest.mock("react-i18next", () => ({
   useTranslation: () => ({ t: (key) => key }),
 }));
+
+const filter = ["eip", "ethereum", "whale", "web3"];
+const baseWidth = "100vw";
+const smWidth = "80vw";
+const mdWidth = "70vw";
+const lgWidth = "670vw";
 
 const loc = window.location;
 
@@ -19,17 +25,13 @@ afterEach(() => {
 
 describe("Search Bar", () => {
   it("It renders the search bar", () => {
-    const baseWidth = "100vw";
-    const smWidth = "80vw";
-    const mdWidth = "70vw";
-    const lgWidth = "670vw";
-
     render(
       <SearchBar
         baseWidth={baseWidth}
         smWidth={smWidth}
         mdWidth={mdWidth}
         lgWidth={lgWidth}
+        filterItems={filter}
       />
     );
 
@@ -41,17 +43,13 @@ describe("Search Bar", () => {
   });
 
   it("It updates the search bar input", () => {
-    const baseWidth = "100vw";
-    const smWidth = "80vw";
-    const mdWidth = "70vw";
-    const lgWidth = "670vw";
-
     render(
       <SearchBar
         baseWidth={baseWidth}
         smWidth={smWidth}
         mdWidth={mdWidth}
         lgWidth={lgWidth}
+        filterItems={filter}
       />
     );
     const searchBarInput = screen.getByTitle("search-bar-input");
@@ -62,17 +60,13 @@ describe("Search Bar", () => {
   });
 
   it("It successfully searches a term when input present", () => {
-    const baseWidth = "100vw";
-    const smWidth = "80vw";
-    const mdWidth = "70vw";
-    const lgWidth = "670vw";
-
     render(
       <SearchBar
         baseWidth={baseWidth}
         smWidth={smWidth}
         mdWidth={mdWidth}
         lgWidth={lgWidth}
+        filterItems={filter}
       />
     );
     const searchBarInput = screen.getByTitle("search-bar-input");
@@ -81,7 +75,7 @@ describe("Search Bar", () => {
     fireEvent.change(searchBarInput, { target: { value: searchTerm } });
     expect(searchBarInput.value).toBe(searchTerm);
 
-    fireEvent.keyPress(searchBarInput, {
+    fireEvent.keyDown(searchBarInput, {
       key: "Enter",
       code: "Enter",
       charCode: 13,
@@ -92,24 +86,224 @@ describe("Search Bar", () => {
     );
   });
 
-  it("It doesn't search a term when input empty", () => {
-    const baseWidth = "100vw";
-    const smWidth = "80vw";
-    const mdWidth = "70vw";
-    const lgWidth = "670vw";
-
+  it("It successfully displays the filter items with user input", () => {
     render(
       <SearchBar
         baseWidth={baseWidth}
         smWidth={smWidth}
         mdWidth={mdWidth}
         lgWidth={lgWidth}
+        filterItems={filter}
+      />
+    );
+    const searchBarInput = screen.getByTitle("search-bar-input");
+    const searchTerm = "e";
+
+    fireEvent.change(searchBarInput, { target: { value: searchTerm } });
+    expect(searchBarInput.value).toBe(searchTerm);
+
+    const autoCompleteFilter = screen.getAllByRole("listitem");
+    expect(autoCompleteFilter.length).toBe(2);
+  });
+
+  it("It successfully cycles through the filter and selects a item for search", () => {
+    render(
+      <SearchBar
+        baseWidth={baseWidth}
+        smWidth={smWidth}
+        mdWidth={mdWidth}
+        lgWidth={lgWidth}
+        filterItems={filter}
+      />
+    );
+    const searchBarInput = screen.getByTitle("search-bar-input");
+    const searchTerm = "e";
+
+    fireEvent.change(searchBarInput, { target: { value: searchTerm } });
+    expect(searchBarInput.value).toBe(searchTerm);
+
+    const autoCompleteFilter = screen.getAllByRole("listitem");
+    expect(autoCompleteFilter.length).toBe(2);
+
+    fireEvent.keyDown(searchBarInput, {
+      key: "ArrowDown",
+      code: "ArrowDown",
+      charCode: 40,
+    });
+
+    fireEvent.keyDown(searchBarInput, {
+      key: "ArrowDown",
+      code: "ArrowDown",
+      charCode: 40,
+    });
+
+    fireEvent.keyDown(searchBarInput, {
+      key: "ArrowUp",
+      code: "ArrowUp",
+      charCode: 38,
+    });
+
+    fireEvent.keyDown(searchBarInput, {
+      key: "Enter",
+      code: "Enter",
+      charCode: 13,
+    });
+
+    expect(window.location.assign).toHaveBeenCalledWith(
+      `/search?term=${autoCompleteFilter[0].textContent}`
+    );
+  });
+
+  it("It successfully rolls around the filter options and searches", () => {
+    render(
+      <SearchBar
+        baseWidth={baseWidth}
+        smWidth={smWidth}
+        mdWidth={mdWidth}
+        lgWidth={lgWidth}
+        filterItems={filter}
+      />
+    );
+    const searchBarInput = screen.getByTitle("search-bar-input");
+    const searchTerm = "e";
+
+    fireEvent.change(searchBarInput, { target: { value: searchTerm } });
+    expect(searchBarInput.value).toBe(searchTerm);
+
+    const autoCompleteFilter = screen.getAllByRole("listitem");
+    expect(autoCompleteFilter.length).toBe(2);
+
+    fireEvent.keyDown(searchBarInput, {
+      key: "ArrowDown",
+      code: "ArrowDown",
+      charCode: 40,
+    });
+
+    fireEvent.keyDown(searchBarInput, {
+      key: "ArrowDown",
+      code: "ArrowDown",
+      charCode: 40,
+    });
+
+    fireEvent.keyDown(searchBarInput, {
+      key: "ArrowDown",
+      code: "ArrowDown",
+      charCode: 40,
+    });
+
+    fireEvent.keyDown(searchBarInput, {
+      key: "Enter",
+      code: "Enter",
+      charCode: 13,
+    });
+
+    expect(window.location.assign).toHaveBeenCalledWith(
+      `/search?term=${autoCompleteFilter[0].textContent}`
+    );
+  });
+
+  it("It successfully searches when filter item clicked", () => {
+    render(
+      <SearchBar
+        baseWidth={baseWidth}
+        smWidth={smWidth}
+        mdWidth={mdWidth}
+        lgWidth={lgWidth}
+        filterItems={filter}
+      />
+    );
+    const searchBarInput = screen.getByTitle("search-bar-input");
+    const searchTerm = "e";
+
+    fireEvent.change(searchBarInput, { target: { value: searchTerm } });
+    expect(searchBarInput.value).toBe(searchTerm);
+
+    const autoCompleteFilter = screen.getAllByRole("listitem");
+    expect(autoCompleteFilter.length).toBe(2);
+
+    const filterSelection = autoCompleteFilter[0].textContent;
+
+    fireEvent.click(autoCompleteFilter[0] , {
+      target: {
+        innerText: filterSelection,
+      },
+    })
+
+    expect(searchBarInput.value).toBe(filterSelection);
+    expect(window.location.assign).toHaveBeenCalledWith(
+      `/search?term=${filterSelection}`
+    );
+  });
+
+  it("It doesn't cycles through the filter when no matches found", () => {
+    render(
+      <SearchBar
+        baseWidth={baseWidth}
+        smWidth={smWidth}
+        mdWidth={mdWidth}
+        lgWidth={lgWidth}
+        filterItems={filter}
+      />
+    );
+    const searchBarInput = screen.getByTitle("search-bar-input");
+    const searchTerm = "z";
+
+    fireEvent.change(searchBarInput, { target: { value: searchTerm } });
+    expect(searchBarInput.value).toBe(searchTerm);
+
+    const autoCompleteFilter = screen.getAllByRole("listitem");
+    expect(autoCompleteFilter.length).toBe(1);
+    expect(autoCompleteFilter[0].textContent).toBe(
+      "searchTermNotFoundInFilter"
+    );
+
+    fireEvent.keyDown(searchBarInput, {
+      key: "ArrowDown",
+      code: "ArrowDown",
+      charCode: 40,
+    });
+  });
+});
+
+describe("Search Bar - preventing bad user input", () => {
+  it("It doesn't search a term when input empty", () => {
+    render(
+      <SearchBar
+        baseWidth={baseWidth}
+        smWidth={smWidth}
+        mdWidth={mdWidth}
+        lgWidth={lgWidth}
+        filterItems={filter}
       />
     );
     const searchBarInput = screen.getByTitle("search-bar-input");
 
     fireEvent.change(searchBarInput, { target: { value: "" } });
     expect(searchBarInput.value).toBe("");
+
+    fireEvent.keyPress(searchBarInput, {
+      key: "Enter",
+      code: "Enter",
+      charCode: 13,
+    });
+
+    expect(window.location.assign).toHaveBeenCalledTimes(0);
+  });
+
+  it("It doesn't search a term when input is whitespace", () => {
+    render(
+      <SearchBar
+        baseWidth={baseWidth}
+        smWidth={smWidth}
+        mdWidth={mdWidth}
+        lgWidth={lgWidth}
+        filterItems={filter}
+      />
+    );
+    const searchBarInput = screen.getByTitle("search-bar-input");
+
+    fireEvent.change(searchBarInput, { target: { value: "   " } });
+    expect(searchBarInput.value).toBe("   ");
 
     fireEvent.keyPress(searchBarInput, {
       key: "Enter",
