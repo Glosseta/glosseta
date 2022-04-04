7584981d5b81db73a6a73fbf06741c2d2efe2f43
naftalimurgor <murgornaftali@gmail.com>
2022-04-04 16:29:34 +0300

    chore: Clean up, better styling with `Responsive styles`

--- src/pages/request-term/index.tsx
+++ src/pages/request-term/index.tsx
@@ -11,101 +11,136 @@ import {
   Input,
   Select,
   Textarea,
-  Button
+  Button,
+  Container,
+  FormLabel
 } from "@chakra-ui/react";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import { useTranslation } from "next-i18next";
 
-type PageStyles = {
-  h1: React.CSSProperties,
-  p: React.CSSProperties,
-  label: React.CSSProperties,
-}
-
-const styles: PageStyles = {
-
-  h1: {
-    fontFamily: "Inter",
-    fontStyle: "normal",
-    fontWeight: "400",
-    fontSize: "48px",
-    lineHeight: "58px",
-    textAlign: "center",
-    color: "#FEF9F9",
-  },
-  p: {
-    width: "332px",
-    height: "25px",
-    fontFamily: "Inter",
-    fontStyle: "normal",
-    fontWeight: "400",
-    fontSize: "14px",
-    lineHeight: "17px",
-    color: "#FEF9F9"
-  },
-
-  label: {
-    position: "absolute",
-    width: "120px",
-    height: "32px",
-    fontFamily: "Inter",
-    fontStyle: "normal",
-    fontWeight: 400,
-    fontSize: "24px",
-    lineHeight: "29px",
-    textAlign: "center",
-    color: "#FFFCFC"
-  }
-
-}
 const RequestTerm = (): JSX.Element => {
   const { t } = useTranslation()
   return (
     <>
       <PageLayout>
         <chakra.main>
-          <Box
-            as="div"
-          >
-            <Center>
-              <Stack>
-                <Heading
-                  style={styles.h1}
-                  as="h1"
-                  title="request-a-term"
-                >
-                  {t("requestATermTitle")}
-                </Heading>
-                <Text style={styles.p}>
-                  {t("requestATermText")}
-                </Text>
-              </Stack>
-            </Center>
-          </Box>
 
-          <Stack spacing={4}>
-            <Center>
-              <VStack spacing={3}>
+          <Container centerContent>
+            <Box
+              as="div"
+              marginBottom="16">
+              <Heading
+                as="h1"
+                textAlign="center"
+                fontSize="48px"
+                lineHeight="58px"
+                color="#FEF9F9"
+                fontStyle="normal"
+                fontWeight="400"
+                title="request-a-term">
+                {t("requestATermTitle")}
+              </Heading>
+              <Text
+                title="request-a-term-description"
+                paddingTop="2"
+                width="332px"
+                height="25px"
+                fontFamily="Inter"
+                fontStyle="normal"
+                fontWeight="400"
+                fontSize="14px"
+                lineHeight="17px"
+                color="#FEF9F9"
+              >
+                {t("requestATermText")}
+              </Text>
+            </Box>
+
+            <Stack spacing={1}
+              paddingLeft={{ base: 10 }}
+              paddingRight={{ base: 10 }}>
+              <Center>
                 <form>
                   <Flex
                     direction="column"
-                  >
-                    <label htmlFor="Term">{t("termLabel")}</label>
-                    <Input />
-                    <label htmlFor="Category">{t("termCategoryLabel")}</label>
-                    <Select placeholder='Select option'>
-                      <option value='option1'>Option 1</option>
-                      <option value='option2'>Option 2</option>
-                      <option value='option3'>Option 3</option>
+                    justifyContent="center"
+                    alignItems="start">
+                    <FormLabel
+                      color="#FFFDFD"
+                      htmlFor="Term"
+                      data-testid="term-label"
+                    >
+                      {t("termLabel")}
+                    </FormLabel>
+                    <Input
+                      data-testid="term-input"
+                      border="1px solid #000000"
+                      boxShadow="0px 4px 4px rgba(0, 0, 0, 0.25)"
+                      id="Term"
+                      backgroundColor="#FFFDFD"
+                      borderRadius="3px"
+                      height="37px"
+                      width={{ base: "300px", md: "526px", lg: "600px" }}
+                      marginTop={{base: 1}}
+                      marginBottom={{ base: 8, md: 6, lg: 4 }} />
+                    <FormLabel
+                      color="#FFFDFD"
+                      htmlFor="Category"
+                      data-testid="category-label">
+                      {t("termCategoryLabel")}
+                    </FormLabel>
+                    <Select
+                      data-testid="term-category-select"
+                      boxShadow="0px 4px 4px rgba(0, 0, 0, 0.25)"
+                      id="Category"
+                      placeholder="Select a Category"
+                      backgroundColor="#FFFDFD"
+                      borderRadius="3px"
+                      height="37px"
+                      width={{ base: "300px", md: "526px", lg: "600px" }}
+                      marginTop="1"
+                      marginBottom={{ base: 8, md: 6, lg: 4 }}>
+                      <option value="general">General</option>
+                      <option value="protocol">Protocol</option>
+                      <option value="dao">DAO</option>
+                      <option value="Token">Token</option>
+                      <option value="application">Application</option>
+                      <option value="finance">Finance</option>
                     </Select>
-                    <label htmlFor="Why should this term be added?">{t("termAdditionReasonLabel")}</label>
-                    <Textarea />
-                    <Button>{t("submitBtnText")}</Button>
+                    <FormLabel
+                      style={{ color: "#FFFDFD" }}
+                      htmlFor="Reason"
+                      data-testid="reason-label">
+                      {t("termAdditionReasonLabel")}
+                    </FormLabel>
+                    <Textarea
+                      data-testid="term-reason"
+                      boxShadow="0px 4px 4px rgba(0, 0, 0, 0.25)"
+                      id="Reason"
+                      backgroundColor="#FFFDFD"
+                      borderRadius="3px"
+                      width={{ base: "300px", md: "526px", lg: "600px" }}
+                      height="164px"
+                      marginTop="1"
+                      marginBottom={{ base: 8, md: 6, lg: 4 }}
+                    />
+                    <Container centerContent marginTop="3">
+                      <Button
+                        data-testid="submit-button"
+                        background="#373636"
+                        color="#FFFEFE"
+                        fontSize="16px"
+                        fontWeight="100px"
+                        fontStyle="normal"
+                        lineHeight="29px">
+                        {t("submitBtnText")}
+                      </Button>
+                    </Container>
                   </Flex>
                 </form>
-              </VStack>
-            </Center>
-          </Stack>
+              </Center>
+            </Stack>
+          </Container>
         </chakra.main>
       </PageLayout>
     </>
