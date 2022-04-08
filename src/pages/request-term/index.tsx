11a0e9b3a68a651b75075e4e88ae4c6a1501d715
naftalimurgor <murgornaftali@gmail.com>
2022-04-09 00:13:52 +0300

    chore: refactor to use `useForm` hook for form validation

--- src/pages/request-term/index.tsx
+++ src/pages/request-term/index.tsx
@@ -1,4 +1,3 @@
-import PageLayout from "../components/layout/page";
 import {
   Box,
   chakra,
@@ -6,41 +5,37 @@ import {
   Heading,
   Text,
   Stack,
-  VStack,
   Flex,
   Input,
   Select,
   Textarea,
   Button,
   Container,
-  FormLabel
+  FormLabel,
+  FormErrorMessage,
+  FormControl,
 } from "@chakra-ui/react";
+
+import { FieldValues, useForm } from "react-hook-form";
+
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import { useTranslation } from "next-i18next";
 import { ChangeEvent, useState } from "react";
 
+import PageLayout from "../components/layout/page";
+
 const RequestTerm = (): JSX.Element => {
 
-  const { t } = useTranslation()
-  type FormEntries = {
-    term: string,
-    category: string,
-    reason: string
-  }
-  const [formEntries, setFormEntries] = useState<FormEntries>({ term: '', category: '', reason: '' })
-  type Event = ChangeEvent<HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement>
+  const { t } = useTranslation();
 
-  const handlechange = (event: Event) => {
-    setFormEntries({
-      ...formEntries,
-      [event.target.name]: event.target.value
-    })
-  }
+  const {
+    handleSubmit,
+    register,
+    formState: { errors, isSubmitting }
+  } = useForm();
 
-  const handleSubmit = (event: ChangeEvent<HTMLFormElement>) => {
-    event.preventDefault()
+  const _onSubmit = (values: FieldValues) => {
 
-    console.log(formEntries)
   }
 
   return (
@@ -82,46 +77,53 @@ const RequestTerm = (): JSX.Element => {
               paddingLeft={{ base: 10 }}
               paddingRight={{ base: 10 }}>
               <Center>
-                <form onSubmit={handleSubmit}>
+                <form onSubmit={handleSubmit<FieldValues>(_onSubmit)}>
                   <Flex
                     direction="column"
                     justifyContent="center"
                     alignItems="start">
+                    <FormControl isInvalid={errors.term}>
+                      <FormLabel
+                        color="#FFFDFD"
+                        htmlFor="term"
+                        data-testid="term-label"
+                      >
+                        {t("termLabel")}
+                      </FormLabel>
+                      <Input
+                        id="term"
+                        data-testid="term-input"
+                        border="1px solid #000000"
+                        boxShadow="0px 4px 4px rgba(0, 0, 0, 0.25)"
+                        backgroundColor="#FFFDFD"
+                        borderRadius="3px"
+                        height="37px"
+                        width={{ base: "300px", md: "526px", lg: "600px" }}
+                        marginTop={{ base: 1 }}
+                        marginBottom={{ base: 8, md: 6, lg: 4 }}
+                        {...register("term", {
+                          required: "This field is required",
+                          minLength: { value: 2, message: "Minimum length should be 2" }
+                        })}
+                      />
+                      <FormErrorMessage>
+                        {errors.term && errors.term.message}
+                      </FormErrorMessage>
+                    </FormControl>
+
                     <FormLabel
+                      marginTop="1"
                       color="#FFFDFD"
-                      htmlFor="Term"
-                      data-testid="term-label"
-                    >
-                      {t("termLabel")}
-                    </FormLabel>
-                    <Input
-                      errorBorderColor="red.300"
-                      value={formEntries.term}
-                      onChange={handlechange}
-                      name="term"
-                      data-testid="term-input"
-                      border="1px solid #000000"
-                      boxShadow="0px 4px 4px rgba(0, 0, 0, 0.25)"
-                      id="Term"
-                      backgroundColor="#FFFDFD"
-                      borderRadius="3px"
-                      height="37px"
-                      width={{ base: "300px", md: "526px", lg: "600px" }}
-                      marginTop={{ base: 1 }}
-                      marginBottom={{ base: 8, md: 6, lg: 4 }} />
-                    <FormLabel
-                      color="#FFFDFD"
-                      htmlFor="Category"
+                      htmlFor="category"
                       data-testid="category-label">
                       {t("termCategoryLabel")}
                     </FormLabel>
+
                     <Select
-                      value={formEntries.category}
-                      onChange={handlechange}
                       name="category"
                       data-testid="term-category-select"
                       boxShadow="0px 4px 4px rgba(0, 0, 0, 0.25)"
-                      id="Category"
+                      id="category"
                       placeholder="Select a Category"
                       backgroundColor="#FFFDFD"
                       borderRadius="3px"
@@ -136,28 +138,37 @@ const RequestTerm = (): JSX.Element => {
                       <option value="application">{t("applicationOption")}</option>
                       <option value="finance">{t("financeOption")}</option>
                     </Select>
-                    <FormLabel
-                      style={{ color: "#FFFDFD" }}
-                      htmlFor="Reason"
-                      data-testid="reason-label">
-                      {t("termAdditionReasonLabel")}
-                    </FormLabel>
-                    <Textarea
-                      value={formEntries.reason}
-                      onChange={handlechange}
-                      name="reason"
-                      data-testid="term-reason"
-                      boxShadow="0px 4px 4px rgba(0, 0, 0, 0.25)"
-                      id="Reason"
-                      backgroundColor="#FFFDFD"
-                      borderRadius="3px"
-                      width={{ base: "300px", md: "526px", lg: "600px" }}
-                      height="164px"
-                      marginTop="1"
-                      marginBottom={{ base: 8, md: 6, lg: 4 }}
-                    />
+
+                    <FormControl isInvalid={errors.reason}>
+                      <FormLabel
+                        style={{ color: "#FFFDFD" }}
+                        htmlFor="reason"
+                        data-testid="reason-label">
+                        {t("termAdditionReasonLabel")}
+                      </FormLabel>
+                      <Textarea
+                        data-testid="term-reason"
+                        boxShadow="0px 4px 4px rgba(0, 0, 0, 0.25)"
+                        id="reason"
+                        backgroundColor="#FFFDFD"
+                        borderRadius="3px"
+                        width={{ base: "300px", md: "526px", lg: "600px" }}
+                        height="164px"
+                        marginTop="1"
+                        marginBottom={{ base: 8, md: 6, lg: 4 }}
+                        {...register("reason", {
+                          required: "This field is required",
+                          minLength: { value: 10, message: "Minimum length should be 10" }
+                        })}
+                      />
+                      <FormErrorMessage>
+                        {errors.reason && errors.reason.message}
+                      </FormErrorMessage>
+                    </FormControl>
+
                     <Container centerContent marginTop="3">
                       <Button
+                        isLoading={isSubmitting}
                         type="submit"
                         data-testid="submit-button"
                         background="#373636"
