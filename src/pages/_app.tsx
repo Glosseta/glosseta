cb10b94c2284a74acebb0ff73bba4ab774d71f15
Narb <narbwork@gmail.com>
2022-05-05 08:47:04 -0700

    Adds new social banner; adds og and twitter meta tags (#169)
    * Adds new social banner; adds og and twitter meta tags
    
    * seo description tweak

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,18 +2,38 @@ import "../../styles/globals.css";
 import type { AppProps } from "next/app";
 import Head from "next/head";
 import { ChakraProvider } from "@chakra-ui/react";
-import { appWithTranslation } from 'next-i18next';
+import { appWithTranslation } from "next-i18next";
+import { useTranslation } from 'next-i18next';
 
 function SEO() {
+  const {t} = useTranslation();
+
   return (
     <Head>
-      <title>Glosseta</title>
+      <meta charSet="utf-8" />
+      <title>{t("glossetaTitle")}</title>
       <meta
         name="description"
-        content="The metaverse's glossary into web3 terms and lingo"
+        content={t("glossetaSEODescription")}
       />
       <link rel="icon" href="/glosseta_icon.png" />
-      <script defer data-domain="glosseta.com" src="https://plausible.io/js/plausible.js"></script>
+      {/* Open Graph / Facebook */}
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content="https://www.glosseta.com" />
+      <meta property="og:title" content={t("glossetaTitle")} />
+      <meta property="og:description" content={t("glossetaSEODescription")}/>
+      <meta property="og:image" content="https://www.glosseta.com/glosseta_social_banner.png" />
+      {/* Twitter */}
+      <meta property="twitter:card" content="summary_large_image" />
+      <meta property="twitter:url" content="https://www.glosseta.com" />
+      <meta property="twitter:title" content={t("glossetaTitle")} />
+      <meta property="twitter:description" content={t("glossetaSEODescription")} />
+      <meta property="twitter:image" content="https://www.glosseta.com/glosseta_social_banner.png" />
+      <script
+        defer
+        data-domain="glosseta.com"
+        src="https://plausible.io/js/plausible.js"
+      ></script>
     </Head>
   );
 }
