b42f7353a76c9adf4d1010b52c76cdcdff1a5c9a
Narbeh Shahnazarian <narbwork@gmail.com>
2022-12-08 22:30:31 -0800

    Updates endpoint for glosseta api

--- src/utils/glosseta-constants.ts
+++ src/utils/glosseta-constants.ts
@@ -2,7 +2,7 @@ export const ARWEAVE_ENDPOINT = "https://arweave.net/";
 export const ARWEAVE_GRAPHQL = "https://arweave.net/graphql";
 export const VIEWBLOCK_URL = "https://viewblock.io/arweave/tx"
 export const GITHUB_ENDPOINT = "https://api.github.com/graphql"
-export const GLOSSETA_API_ENDPOINT = "https://content.glosseta.dev/api/graphql";
+export const GLOSSETA_API_ENDPOINT = "https://perma.storedat.io/api/graphql";
 
 export const TERM_TAG = "term" as String;
 export const DESCRIPTION_TAG = "description" as String;
