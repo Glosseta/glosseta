5cb8b9fc3fccabf98b8c89b7b3587a508e4873b8
Narbeh Shahnazarian <narbwork@gmail.com>
2022-12-11 18:27:09 -0800

    updates storedat api route to use v1

--- src/utils/glosseta-constants.ts
+++ src/utils/glosseta-constants.ts
@@ -2,7 +2,7 @@ export const ARWEAVE_ENDPOINT = "https://arweave.net/";
 export const ARWEAVE_GRAPHQL = "https://arweave.net/graphql";
 export const VIEWBLOCK_URL = "https://viewblock.io/arweave/tx"
 export const GITHUB_ENDPOINT = "https://api.github.com/graphql"
-export const GLOSSETA_API_ENDPOINT = "https://perma.storedat.io/api/graphql";
+export const GLOSSETA_API_ENDPOINT = "https://perma.storedat.io/api/v1/graphql";
 
 export const TERM_TAG = "term" as String;
 export const DESCRIPTION_TAG = "description" as String;
